33d2d6c19b9db8df02aac40660c8c1e30c1d9443
Yoshitaka Totsuka <y047aka@gmail.com>
2025-01-16 19:56:21 +0900

    feat: About と Overview のマークアップを移植

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -4,7 +4,8 @@ import BackendTask exposing (BackendTask)
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
-import Html exposing (h1, p, text)
+import Html exposing (Html, div, h1, h2, h3, iframe, p, section, text)
+import Html.Attributes exposing (attribute, class, height, src, style)
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
 import Route
@@ -71,6 +72,10 @@ head app =
         |> Seo.website
 
 
+
+-- VIEW
+
+
 view :
     App Data ActionData RouteParams
     -> Shared.Model
@@ -84,5 +89,58 @@ view app shared =
             ]
         , Route.Blog__Slug_ { slug = "hello" }
             |> Route.link [] [ text "My blog post" ]
+        , aboutBlock
+        , overviewBlock
         ]
     }
+
+
+aboutBlock : Html msg
+aboutBlock =
+    block "About"
+        [ p [] [ text "関数型プログラミングのカンファレンス「関数型まつり」を開催します！" ]
+        , p []
+            [ text "関数型プログラミングはメジャーな言語・フレームワークに取り入れられ、広く使われるようになりました。"
+            , text "そしてその手法自体も進化し続けています。"
+            , text "その一方で「関数型プログラミング」というと「難しい・とっつきにくい」という声もあり、十分普及し切った状態ではありません。"
+            ]
+        , p []
+            [ text "私たちは様々な背景の方々が関数型プログラミングを通じて新しい知見を得て、交流ができるような場を提供することを目指しています。"
+            , text "普段から関数型言語を活用している方や関数型プログラミングに興味がある方はもちろん、最先端のソフトウェア開発技術に興味がある方もぜひご参加ください！"
+            ]
+        ]
+
+
+overviewBlock : Html msg
+overviewBlock =
+    block "Overview"
+        [ div [ class "md:flex flex-wrap justify-between gap-x-8" ]
+            [ div [ style "min-width" "18rem" ]
+                [ h3 [ class "font-semibold" ] [ text "Dates" ]
+                , p [] [ text "2025.6.14(土)〜15(日)" ]
+                ]
+            , div [ style "min-width" "18rem" ]
+                [ h3 [ class "font-semibold" ] [ text "Place" ]
+                , p [] [ text "中野セントラルパーク カンファレンス" ]
+                ]
+            ]
+        , iframe
+            [ src "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25918.24822641297!2d139.64379899847268!3d35.707005772578796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f34668e0bc27%3A0x7d66caba722762c5!2z5Lit6YeO44K744Oz44OI44Op44Or44OR44O844Kv44Kr44Oz44OV44Kh44Os44Oz44K5!5e0!3m2!1sen!2sjp!4v1736684092765!5m2!1sen!2sjp"
+            , attribute "width" "100%"
+            , height 400
+            , style "border" "0"
+            , attribute "allowfullscreen" ""
+            , attribute "loading" "lazy"
+            , attribute "referrerpolicy" "no-referrer-when-downgrade"
+            ]
+            []
+        ]
+
+
+block : String -> List (Html msg) -> Html msg
+block title children =
+    let
+        heading =
+            h2 [] [ text title ]
+    in
+    section [] (heading :: children)
