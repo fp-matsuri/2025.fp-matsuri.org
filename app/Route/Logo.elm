34162ce37497d1074937c001380a63717bdf46e8
Yoshitaka Totsuka <y047aka@gmail.com>
2025-02-13 16:43:59 +0900

    feat: `clip-path` を活用する

--- app/Route/Logo.elm
+++ app/Route/Logo.elm
@@ -84,25 +84,30 @@ logoMark color1 color2 =
     let
         id_ =
             color1 ++ "_" ++ color2
-    in
-    svg [ width "125", height "100", viewBox "0 0 250 200", attribute "xmlns" "http://www.w3.org/2000/svg" ]
-        [ defs []
-            [ linearGradient [ id id_, gradientTransform "rotate(45)" ]
+
+        clipPath =
+            Svg.clipPath [ id "logo_outline" ]
+                [ path [ d "M0 50C0 22.3858 22.3858 0 50 0V50H0Z" ] []
+                , rect [ width "50", height "50", transform "translate(50)" ] []
+                , rect [ width "50", height "50", transform "translate(150)" ] []
+                , rect [ width "50", height "50", transform "translate(0 50)" ] []
+                , rect [ width "50", height "50", transform "translate(100 50)" ] []
+                , rect [ x "200", y "50", width "50", height "50", rx "25" ] []
+                , rect [ width "50", height "50", transform "translate(0 100)" ] []
+                , rect [ width "50", height "50", transform "translate(50 100)" ] []
+                , rect [ width "50", height "50", transform "translate(150 100)" ] []
+                , rect [ width "50", height "50", transform "translate(0 150)" ] []
+                , rect [ width "50", height "50", transform "translate(100 150)" ] []
+                ]
+
+        gradient =
+            linearGradient [ id ("gradient_" ++ id_), gradientTransform "rotate(45)" ]
                 [ stop [ offset "0%", stopColor color1 ] []
                 , stop [ offset "100%", stopColor color2 ] []
                 ]
-            ]
-        , g [ fill ("url(#" ++ id_ ++ ")") ]
-            [ path [ d "M0 50C0 22.3858 22.3858 0 50 0V50H0Z" ] []
-            , rect [ width "50", height "50", transform "translate(50)" ] []
-            , rect [ width "50", height "50", transform "translate(150)" ] []
-            , rect [ width "50", height "50", transform "translate(0 50)" ] []
-            , rect [ width "50", height "50", transform "translate(100 50)" ] []
-            , rect [ x "200", y "50", width "50", height "50", rx "25" ] []
-            , rect [ width "50", height "50", transform "translate(0 100)" ] []
-            , rect [ width "50", height "50", transform "translate(50 100)" ] []
-            , rect [ width "50", height "50", transform "translate(150 100)" ] []
-            , rect [ width "50", height "50", transform "translate(0 150)" ] []
-            , rect [ width "50", height "50", transform "translate(100 150)" ] []
-            ]
+    in
+    svg [ width "125", height "100", viewBox "0 0 250 200", attribute "xmlns" "http://www.w3.org/2000/svg" ]
+        [ defs [] [ clipPath, gradient ]
+        , g [ Svg.Attributes.clipPath "url(#logo_outline)", fill ("url(#gradient_" ++ id_ ++ ")") ]
+            [ rect [ width "250", height "200" ] [] ]
         ]
