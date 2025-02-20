eb37aa51d4a97c87d61cb97eb604273fd4eb0e5b
Yoshitaka Totsuka <y047aka@gmail.com>
2025-02-20 13:39:23 +0900

    feat: ロゴの形状を更新（2月19日）

--- src/FPMatsuri/Logo.elm
+++ src/FPMatsuri/Logo.elm
@@ -13,17 +13,17 @@ logoMark index ( color1, color2 ) =
 
         clipPath =
             Svg.clipPath [ id "logo_outline" ]
-                [ path [ d "M0 50C0 22.3858 22.3858 0 50 0V50H0Z" ] []
-                , rect [ width "50", height "50", transform "translate(50)" ] []
-                , rect [ width "50", height "50", transform "translate(150)" ] []
+                [ path [ d "M0 50C0 22.3858 22.3858 0 50 0V0V50H0V50Z" ] []
+                , path [ d "M50 0H100V40C100 45.5228 95.5228 50 90 50H50V0Z" ] []
+                , path [ d "M150 0H190C195.523 0 200 4.47715 200 10V40C200 45.5228 195.523 50 190 50H160C154.477 50 150 45.5228 150 40V0Z" ] []
                 , rect [ width "50", height "50", transform "translate(0 50)" ] []
-                , rect [ width "50", height "50", transform "translate(100 50)" ] []
+                , path [ d "M100 50H140C145.523 50 150 54.4772 150 60V90C150 95.5228 145.523 100 140 100H100V50Z" ] []
                 , rect [ x "200", y "50", width "50", height "50", rx "25" ] []
                 , rect [ width "50", height "50", transform "translate(0 100)" ] []
-                , rect [ width "50", height "50", transform "translate(50 100)" ] []
-                , rect [ width "50", height "50", transform "translate(150 100)" ] []
+                , path [ d "M50 100H90C95.5228 100 100 104.477 100 110V140C100 145.523 95.5228 150 90 150H50V100Z" ] []
+                , path [ d "M150 110C150 104.477 154.477 100 160 100H190C195.523 100 200 104.477 200 110V140C200 145.523 195.523 150 190 150H150V110Z" ] []
                 , rect [ width "50", height "50", transform "translate(0 150)" ] []
-                , rect [ width "50", height "50", transform "translate(100 150)" ] []
+                , path [ d "M100 150H140C145.523 150 150 154.477 150 160V200H100V150Z" ] []
                 ]
 
         gradient =
