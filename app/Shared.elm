2564824dd1f7daa2bacc73567cf57097251f2189
Yoshitaka Totsuka <y047aka@gmail.com>
2025-02-24 18:11:12 +0900

    feat: オリジナルの配色を `logoMark` として設定

--- app/Shared.elm
+++ app/Shared.elm
@@ -93,7 +93,7 @@ view _ _ _ _ pageView =
         [ header [ class "site-header" ]
             [ h1 []
                 [ a [ href "/" ]
-                    [ FPMatsuri.logoMarkWithOptions -1 ( "#FC0", "#C00" )
+                    [ FPMatsuri.logoMark
                     , text "関数型まつり"
                     ]
                 ]
