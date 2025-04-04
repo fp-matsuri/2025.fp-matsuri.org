c97aa18e0351e197dbfcbc095c3e65445f227937
Keita-N <Keita-N@users.noreply.github.com>
2025-04-04 23:32:39 +0900

    gridでコンテンツを配置

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -237,7 +237,7 @@ overviewSection =
                 []
     in
     section "Overview"
-        [ div [ class "overview-flex-box" ] [ information, map ]
+        [ div [ class "overview-box" ] [ information, map ]
         ]
 
 
