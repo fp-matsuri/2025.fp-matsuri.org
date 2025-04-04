de77bb3684db2c94c457f054517a609e63f37fc7
Keita-N <Keita-N@users.noreply.github.com>
2025-04-04 15:17:03 +0900

    コンテンツを更新
    - markdown クラス適用を削除

--- app/Route/Index.elm
+++ app/Route/Index.elm
@@ -202,17 +202,23 @@ overviewSection =
             itemHeader key (p [] [ text value ])
 
         information =
-            div [ class "markdown overview" ]
-                [ item "Dates"
-                    "2025.6.14(土)〜15(日)"
-                , item "Place"
+            div [ class "overview" ]
+                [ itemHeader "日程"
+                    (ul []
+                        [ li [] [ text "1日目：6月14日（土）11:00〜19:00" ]
+                        , li [] [ text "2日目：6月15日（日）10:00〜18:00" ]
+                        ]
+                    )
+                , item "会場"
                     "中野セントラルパーク カンファレンス"
-                , itemHeader "Ticket"
-                    (div [ class "ticket" ]
+                , itemHeader "チケット"
+                    (div []
                         [ ul []
                             [ li [] [ text "一般チケット(懇親会なし) 3,000円" ]
                             , li [] [ text "一般チケット(懇親会あり) 8,000円" ]
                             ]
+                        , text "※ 1日目のセッション終了後には、参加者同士の交流を深める懇親会を予定しております。参加される方は「懇親会あり」のチケットをご購入ください。"
+                        , a [ href "#" ] [ p [ class "link-to-doorkeeper" ] [ text "チケット販売サイト（Doorkeeper）" ] ]
                         ]
                     )
                 ]
