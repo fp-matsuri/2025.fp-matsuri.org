b058d1fd10299c19c260b9e7428d15c6479d7d03
Yoshitaka Totsuka <y047aka@gmail.com>
2025-04-13 00:37:30 +0900

    feat: Trackの振り分けを指定

--- app/Route/Schedule.elm
+++ app/Route/Schedule.elm
@@ -3,7 +3,7 @@ module Route.Schedule exposing (ActionData, Data, Model, Msg, route)
 import BackendTask exposing (BackendTask)
 import BackendTask.Http
 import Css exposing (..)
-import Css.Extra exposing (fr, gap, grid, gridTemplateColumns)
+import Css.Extra exposing (fr, gap, grid, gridColumn, gridTemplateColumns)
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo
@@ -151,6 +151,201 @@ trackDecoder =
         (field "sort" Decode.int)
 
 
+trackFromUuid : String -> Track
+trackFromUuid uuid =
+    case uuid of
+        -- Haskell でアルゴリズムを抽象化する 〜 関数型言語で競技プログラミング
+        "ad0d29f8-46a2-463b-beeb-39257f9c5306" ->
+            { name = "Track A", sort = 200 }
+
+        -- ClojureScript (Squint) で React フロントエンド開発 2025 年版
+        "e7f30174-d4b9-40a7-9398-9f15c71009a9" ->
+            { name = "Track B", sort = 200 }
+
+        -- Lispは関数型言語(ではない)
+        "92b697d1-206c-426a-90c9-9ff3486cce6f" ->
+            { name = "Track B", sort = 200 }
+
+        -- Lean言語は新世代の純粋関数型言語になれるか？
+        "73b09de0-c72e-4bbd-9089-af5c002f9506" ->
+            { name = "Track B", sort = 200 }
+
+        -- Rust世界の二つのモナド──Rust でも do 式をしてプログラムを直感的に記述する件について
+        "a8cd6d02-37c5-4009-90a4-9495c3189420" ->
+            { name = "Track A", sort = 100 }
+
+        -- はじめて関数型言語の機能に触れるエンジニア向けの学び方/教え方
+        "7cc6ecef-94c8-4add-abc0-23b500dbf498" ->
+            { name = "Track C", sort = 200 }
+
+        -- Julia という言語について
+        "4ca1dabd-dbbe-47ca-a813-bc4c9700ccc9" ->
+            { name = "Track B", sort = 200 }
+
+        -- 「Haskellは純粋関数型言語だから副作用がない」っていうの、そろそろ止めにしませんか？
+        "d19de11e-d9a2-4b22-866e-2f95b8ac5c95" ->
+            { name = "Track B", sort = 200 }
+
+        -- 型付きアクターモデルがもたらす分散シミュレーションの未来
+        "82478074-a43b-4d46-87a8-0742ed790e86" ->
+            { name = "Track B", sort = 200 }
+
+        -- Kotlinで学ぶSealed classと代数的データ型
+        "e436393d-c322-477d-b8cb-0e6ac8ce8cc6" ->
+            { name = "Track B", sort = 200 }
+
+        -- ラムダ計算って何だっけ？関数型の神髄に迫る
+        "81cea14c-255c-46ff-929d-5141c5715832" ->
+            { name = "Track C", sort = 200 }
+
+        -- 関数型言語テイスティング: Haskell, Scala, Clojure, Elixirを比べて味わう関数型プログラミングの旨さ
+        "f7646b8b-29b0-4ac4-8ec3-46cabaa8ef1a" ->
+            { name = "Track C", sort = 100 }
+
+        -- SML＃ オープンコンパイラプロジェクト
+        "61fb241f-cfaa-448a-892d-277e93577198" ->
+            { name = "Track A", sort = 200 }
+
+        -- 型システムを知りたい人のための型検査器作成入門
+        "5699c262-e04d-4f58-a6f5-34c390f36d0d" ->
+            { name = "Track A", sort = 100 }
+
+        -- Effectの双対、Coeffect
+        "67557418-7561-47ec-8594-9d6c0926a6ab" ->
+            { name = "Track A", sort = 100 }
+
+        -- 成立するElixirの再束縛（再代入）可という選択
+        "8acfb03f-19ea-476a-b6e6-0cb4b03fec1f" ->
+            { name = "Track B", sort = 200 }
+
+        -- continuations: continued and to be continued
+        "ea9fd8fc-4ae3-40c7-8ef5-1a8041e64606" ->
+            { name = "Track A", sort = 200 }
+
+        -- Leanで正規表現エンジンをつくる。そして正しさを証明する
+        "af94193a-4acb-4079-82a9-36bacfae3a20" ->
+            { name = "Track B", sort = 200 }
+
+        -- F#の設計と妥協点 - .NET上で実現する関数型パラダイム
+        "a916dd5a-7342-416a-980d-84f180a8e0a2" ->
+            { name = "Track C", sort = 200 }
+
+        -- Excelで関数型プログラミング
+        "37899705-7d88-4ca4-bd5b-f674fc372d4e" ->
+            { name = "Track C", sort = 200 }
+
+        -- iOSアプリ開発で関数型プログラミングを実現するThe Composable Architectureの紹介
+        "71fbd521-9dc5-458d-89f6-cbff8e84e3cc" ->
+            { name = "Track C", sort = 200 }
+
+        -- Gleamという選択肢
+        "e9df1f36-cf2f-4a85-aa36-4e07ae742a69" ->
+            { name = "Track A", sort = 200 }
+
+        -- Scalaだったらこう書けるのに~Scalaが恋しくて~(TypeScript編、Python編)
+        "2ceb7498-b203-44ee-b064-c0fbbe4a6948" ->
+            { name = "Track B", sort = 200 }
+
+        -- Elixir で IoT 開発、 Nerves なら簡単にできる！？
+        "b952a4f0-7db5-4d67-a911-a7a5d8a840ac" ->
+            { name = "Track B", sort = 100 }
+
+        -- Hasktorchで学ぶ関数型ディープラーニング：型安全なニューラルネットワークとその実践
+        "b7a97e49-8624-4eae-848a-68f70205ad2a" ->
+            { name = "Track B", sort = 100 }
+
+        -- CoqのProgram機構の紹介 〜型を活用した安全なプログラミング〜
+        "983d1021-3636-4778-be58-149f1995e8a5" ->
+            { name = "Track C", sort = 200 }
+
+        -- デコーダーパターンによる3Dジオメトリの読み込み
+        "a82127a7-f84a-43c1-a3de-483e1d973a94" ->
+            { name = "Track C", sort = 200 }
+
+        -- 産業機械をElixirで制御する
+        "6edaa6b5-b591-490c-855f-731a9d318192" ->
+            { name = "Track B", sort = 100 }
+
+        -- 「ElixirでIoT!!」のこれまでとこれから
+        "f75e5cab-c677-44bb-a77a-2acf36083457" ->
+            { name = "Track B", sort = 100 }
+
+        -- 高階関数を用いたI/O方法の公開 - DIコンテナから高階関数への更改 -
+        "350e2f70-0b02-4b79-b9f6-254a9d614706" ->
+            { name = "Track C", sort = 100 }
+
+        -- XSLTで作るBrainfuck処理系 ― XSLTは関数型言語たり得るか？
+        "8dcaecb5-4541-4262-a047-3e330a7bcdb8" ->
+            { name = "Track C", sort = 200 }
+
+        -- 数理論理学からの『型システム入門』入門？
+        "a6badfbb-ca70-474d-9abd-f285f24d9380" ->
+            { name = "Track A", sort = 200 }
+
+        -- Underground 型システム
+        "e0274da9-d863-47fe-a945-42eb04185bb9" ->
+            { name = "Track C", sort = 200 }
+
+        -- 関数プログラミングに見る再帰
+        "034e486c-9a1c-48d7-910a-14aa82237eaa" ->
+            { name = "Track A", sort = 200 }
+
+        -- より安全で単純な関数定義
+        "75644660-9bf1-473f-8d6d-01f2202bf2f2" ->
+            { name = "Track A", sort = 200 }
+
+        -- Scala の関数型ライブラリを活用した型安全な業務アプリケーション開発
+        "02f89c3a-672e-4294-ae31-69e02e049005" ->
+            { name = "Track A", sort = 200 }
+
+        -- 関数型言語を採用し、維持し、継続する
+        "76a0de1e-bf79-4c82-b50e-86caedaf1eb9" ->
+            { name = "Track A", sort = 100 }
+
+        -- ドメインモデリングにおける抽象の役割、tagless-finalによるDSL構築、そして型安全な最適化
+        "f3a8809b-d498-4ac2-bf42-5c32ce1595ea" ->
+            { name = "Track C", sort = 100 }
+
+        -- 堅牢な認証基盤の実現: TypeScriptで代数的データ型を活用する
+        "267ff4c1-8f3c-473b-8cab-e62d0d468af5" ->
+            { name = "Track C", sort = 200 }
+
+        -- SML#コンパイラを速くする：タスク並列、末尾呼び出し、部分評価機構の開発
+        "b69688cf-06a2-4070-839c-4a6ec299c39c" ->
+            { name = "Track B", sort = 200 }
+
+        -- AWS と定理証明 〜ポリシー言語 Cedar 開発の舞台裏〜
+        "8bb407b5-5df3-48bb-a934-0ca6ca628c9a" ->
+            { name = "Track A", sort = 100 }
+
+        -- F#で自在につくる静的ブログサイト
+        "b6c70e2d-856b-47c5-9107-481883527634" ->
+            { name = "Track C", sort = 200 }
+
+        -- Elmのパフォーマンス、実際どうなの？ベンチマークに入門してみた
+        "3760ed3e-5b38-48b9-9db2-f101af1e580f" ->
+            { name = "Track C", sort = 100 }
+
+        -- マイクロサービス内で動くAPIをF#で書いている
+        "7a342a71-90d4-43f9-9c4a-ce801fc9b49a" ->
+            { name = "Track C", sort = 200 }
+
+        -- ラムダ計算と抽象機械と非同期ランタイム
+        "3bdbadb9-7d77-4de0-aa37-5a7a38c577c3" ->
+            { name = "Track A", sort = 200 }
+
+        -- `interact`のススメ — できるかぎり「関数的」に書きたいあなたに
+        "6109f011-c590-4c89-9add-89ad12cc9631" ->
+            { name = "Track B", sort = 100 }
+
+        -- AIと共に進化する開発手法：形式手法と関数型プログラミングの可能性
+        "56b9175d-1468-4ab0-8063-180491bb16ed" ->
+            { name = "Track C", sort = 100 }
+
+        _ ->
+            { name = "Track D", sort = 1 }
+
+
 head : App Data ActionData RouteParams -> List Head.Tag
 head _ =
     Site.summaryLarge { pageTitle = "開催スケジュール" }
@@ -191,13 +386,15 @@ viewTimetableItem timetableItem =
         Talk talk ->
             div
                 [ css
-                    [ padding (px 10)
+                    [ gridColumn (columnFromTrack (trackFromUuid talk.uuid))
+                    , padding (px 10)
                     , borderRadius (px 10)
                     , fontSize (px 14)
                     , property "background-color" "var(--color-grey095)"
                     ]
                 ]
-                [ div [ css [ Css.marginBottom (Css.px 8) ] ]
+                [ text (trackFromUuid talk.uuid |> .name)
+                , div [ css [ Css.marginBottom (Css.px 8) ] ]
                     [ text talk.title ]
                 , div [ css [ Css.color (Css.rgb 75 85 99) ] ]
                     [ text ("発表者: " ++ talk.speaker.name) ]
@@ -206,12 +403,30 @@ viewTimetableItem timetableItem =
         Timeslot timeslot ->
             div
                 [ css
-                    [ padding (px 10)
+                    [ gridColumn (columnFromTrack timeslot.track)
+                    , padding (px 10)
                     , borderRadius (px 10)
                     , fontSize (px 14)
                     , property "background-color" "var(--color-grey095)"
                     ]
                 ]
-                [ div [ css [ Css.marginBottom (Css.px 8) ] ]
+                [ text timeslot.track.name
+                , div [ css [ Css.marginBottom (Css.px 8) ] ]
                     [ text timeslot.title ]
                 ]
+
+
+columnFromTrack : Track -> String
+columnFromTrack track =
+    case track.name of
+        "Track A" ->
+            "1"
+
+        "Track B" ->
+            "2"
+
+        "Track C" ->
+            "3"
+
+        _ ->
+            "4"
